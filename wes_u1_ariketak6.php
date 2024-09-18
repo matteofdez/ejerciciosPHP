822b2f625994551174327ce90a64146fff291cbc
Matteo <6ikasle@gmail.com>
2024-09-18 10:44:45 +0200

    ejercicioss

---
+++ wes_u1_ariketak6.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <!--Tabla borderrekin-->
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+    </style>
+    <!--Array Multidimentsionala Bistaratu-->
+    <h1>Ariketa 5.1</h1>
+    <table>
+        <tr>
+            <th>Izena</th>
+            <th>Autorea</th>
+        </tr>
+        <?php
+     
+        ?>
+    </table>
+  
+
+
+</body>
+
+</html>
