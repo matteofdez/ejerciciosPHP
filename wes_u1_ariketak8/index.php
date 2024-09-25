26c79b80fc61a6b084ffb3204c5060aebc588f3b
Matteo <6ikasle@gmail.com>
2024-09-25 10:56:41 +0200

    titulo cambiado

--- wes_u1_ariketak8/index.php
+++ wes_u1_ariketak8/index.php
@@ -68,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulagailua</title>
+    <title>Ariketa 8</title>
 </head>
 <body>
     <h2>Kalkulagailua</h2>
