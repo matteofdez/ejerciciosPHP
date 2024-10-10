874c80d815835c59c9ab6acf71c88de96c9597a8
Matteo <6ikasle@gmail.com>
2024-10-10 12:35:42 +0200

    JSON

--- wes_u1_ariketak12/taldeak_api.php
+++ wes_u1_ariketak12/taldeak_api.php
@@ -3,13 +3,68 @@ require 'db.php';
 require 'taldea.php';
 require 'taldeaDB.php';
 
-    // Bueltatuko den erantzuna JSON formatuan egongo dela adierazi.
-    header("Content-Type: application/json");
+// Bueltatuko den erantzuna JSON formatuan egongo dela adierazi.
+header("Content-Type: application/json");
+$db = new DB();
+$db->konektatu();
+$taldeaDB = new TaldeaDB($db);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
-    if ($_SERVER["REQUEST_METHOD"]=="GET") {
-        $db = new DB();
-        $db->konektatu();
-        $taldeaDB = new TaldeaDB($db);
-        $respuesta=$taldeaDB->getAll();
+    if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+        $respuesta = $taldeaDB->get($id);
+        echo json_encode($respuesta);
+    } else {
+        $respuesta = $taldeaDB->getAll();
         echo json_encode($respuesta);
     }
+}
+elseif ($_SERVER["REQUEST_METHOD"]== "POST"){
+    // Dekodifikatu jasotako JSON informazioa.
+    $aux = json_decode(file_get_contents('php://input'), true);
+    $izenaErr = "";
+    if (empty($aux["izena"])) {
+        $izenaErr = "Izena derrigorrezkoa da.";
+    } else {
+        $izena = htmlspecialchars($aux["izena"]);
+    }
+
+    if (!empty($aux["puntuak"])) {
+        $puntuak = (int)htmlspecialchars($aux["puntuak"]);
+    }
+    print_r($izenaErr);
+    if (empty($izenaErr)) {
+        if($taldeaDB->create($izena, $puntuak)){
+            $respuesta = $taldeaDB->getAll();
+            echo json_encode($respuesta);
+        }
+        else {
+            $respuesta = $taldeaDB->getAll();
+            echo json_encode($respuesta);
+        }
+    }
+}
+elseif($_SERVER["REQUEST_METHOD"]=="PUT"){ 
+    $aux = json_decode(file_get_contents('php://input'), true);
+       
+        if($taldeaDB->editPuntuak($aux['id'], $aux['puntuak'])) {
+            $respuesta = $taldeaDB->getAll();
+            echo json_encode($respuesta);
+        } else {
+            $respuesta = $taldeaDB->getAll();
+            echo json_encode($respuesta);
+        }
+}
+elseif($_SERVER["REQUEST_METHOD"]=="DELETE"){
+    $aux = json_decode(file_get_contents('php://input'), true);
+        if($taldeaDB->delete($aux['id'])) {
+            $respuesta = $taldeaDB->getAll();
+            echo json_encode($respuesta);
+        }
+        else {
+            $respuesta = $taldeaDB->getAll();
+            echo json_encode($respuesta);
+        }
+}
+
+
