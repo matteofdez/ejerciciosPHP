8fe81c861b1c8ef291ad9639b55dcd03745a2c40
Matteo <6ikasle@gmail.com>
2025-01-14 10:54:48 +0100

    a

---
+++ E2_Ariketa_3/app.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        Erabiltzaile : [
+            {
+              id: 1,
+              izena: "Jon",
+              abizena: "Ortiz",
+              NAN: "12345678-A",
+              jaiotzeData: "12-04-67",
+              adina: 52
+            },
+            {
+              id: 2,
+              izena: "Peru",
+              abizena: "Felix",
+              NAN: "12367543-B",
+              jaiotzeData: "30-11-90",
+              adina: 29
+            },
+            {
+              id: 3,
+              izena: "Maddi",
+              abizena: "Azkona",
+              NAN: "12356234-c",
+              jaiotzeData: "02-12-59",
+              adina: 60
+            }
+          ]
+          
+    }
+})
